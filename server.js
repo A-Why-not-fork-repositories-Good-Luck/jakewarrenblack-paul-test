a82a05d2480d013fb65b4c22d7f59fc65bc6ca96
jakewarrenblack <jake.black@sonalake.com>
2022-12-01 16:42:52 +0000

    Jake test

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const app = express();
-const port = 3000;
-
-
+const port = 3000;git
 require("dotenv").config();
-
 require("./utils/db.js")();
-
 app.use(express.json());
-
-
-
 app.use(express.static("public"));
+const cors = require('cors')
+app.use(cors())
 
 app.use((req, res, next) => {
   if (req.headers?.authorization?.split(" ")[0] === "Bearer") {
